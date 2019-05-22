e5f22a2543fbe1d1021fa5135edc315fb6e21d4b
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-05-21 18:11:05 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    j = 0
+    new_collection= []
+    while i < collection.length
+        if collection[i] % 2 == 0
+            new_collection.push(collection[i])
+        end
+       
+        i += 1
+    end
+    while j < new_collection.length
+        yield new_collection[j]
+        j += 1
+    end
+
+    new_collection
 end
