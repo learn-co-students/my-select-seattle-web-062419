9fe66ac4ba6387088f17adbabdd03b99dfdd9b75
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-05-21 22:53:35 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,17 +1,11 @@
 def my_select(collection)
     i = 0
-    j = 0
     new_collection= []
     while i < collection.length
-        if collection[i] % 2 == 0
+        if yield collection[i]
             new_collection.push(collection[i])
         end
-       
         i += 1
-    end
-    while j < new_collection.length
-        yield new_collection[j]
-        j += 1
     end
 
     new_collection
